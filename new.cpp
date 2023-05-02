751dc375be93750f19028ff0a690c2d696801505
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2023-05-03 00:50:34 +0530

    This is it

---
+++ new.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is just a website to make changes in github" << endl;
+    cout << "You can change this file as you want" << endl;
+}
